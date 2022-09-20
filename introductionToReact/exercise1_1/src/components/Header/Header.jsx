8f80ea21416e0ce4761b2bf87293fad815281901
soulaymanegharroudi <soulaymane.gharroudi@student.vinci.be>
2022-09-20 15:30:11 +0200

    1.1-1.2 : import d'image

--- introductionToReact/exercise1_1/src/components/Header/Header.jsx
+++ introductionToReact/exercise1_1/src/components/Header/Header.jsx
@@ -1,7 +1,9 @@
 import './Header.css'
+import logo from './LOGO HE VINCI.png'
 const Header = (props) => {
   return (
     <div className={"header"}>
+      <img src={logo}/>
       <h1>{props.course}</h1>
     </div>
   )
